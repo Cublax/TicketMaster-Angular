e5ace74eed8b8a4e0278d351ed62c34808c52798
Alexandre Quiblier <alexandre.quiblier@meona.de>
2023-05-30 09:26:27 +0200

    adding last date to commit

--- src/app/TicketMasterData.ts
+++ src/app/TicketMasterData.ts
@@ -1,3 +1,4 @@
+
 export interface TicketMasterData {
   _embedded: Embedded
   _links: Links4
@@ -21,9 +22,19 @@ export interface Event {
   classifications: Classification[]
   promoter: Promoter
   promoters: Promoter2[]
-  priceRanges: PriceRange[]
-  seatmap?: Seatmap
+  priceRanges?: PriceRange[]
+  seatmap: Seatmap
+  accessibility?: Accessibility
+  ticketLimit?: TicketLimit
+  ageRestrictions?: AgeRestrictions
+  ticketing?: Ticketing
+  _links: Links
   _embedded: Embedded2
+  pleaseNote?: string
+  products?: Product[]
+  info?: string
+  outlets?: Outlet[]
+  doorsTimes?: DoorsTimes
 }
 
 export interface Image {
@@ -36,6 +47,7 @@ export interface Image {
 
 export interface Sales {
   public: Public
+  presales?: Presale[]
 }
 
 export interface Public {
@@ -45,9 +57,16 @@ export interface Public {
   endDateTime: string
 }
 
+export interface Presale {
+  startDateTime: string
+  endDateTime: string
+  name: string
+  description?: string
+}
+
 export interface Dates {
   start: Start
-  timezone: string
+  timezone?: string
   status: Status
   spanMultipleDays: boolean
 }
@@ -71,6 +90,8 @@ export interface Classification {
   segment: Segment
   genre: Genre
   subGenre: SubGenre
+  type?: Type
+  subType?: SubType
   family: boolean
 }
 
@@ -89,14 +110,26 @@ export interface SubGenre {
   name: string
 }
 
+export interface Type {
+  id: string
+  name: string
+}
+
+export interface SubType {
+  id: string
+  name: string
+}
+
 export interface Promoter {
   id: string
   name: string
+  description: string
 }
 
 export interface Promoter2 {
   id: string
   name: string
+  description: string
 }
 
 export interface PriceRange {
@@ -109,6 +142,36 @@ export interface PriceRange {
 export interface Seatmap {
   staticUrl: string
 }
+
+export interface Accessibility {
+  ticketLimit: number
+  info?: string
+  url?: string
+  urlText?: string
+}
+
+export interface TicketLimit {
+  info: string
+}
+
+export interface AgeRestrictions {
+  legalAgeEnforced: boolean
+}
+
+export interface Ticketing {
+  safeTix: SafeTix
+}
+
+export interface SafeTix {
+  enabled: boolean
+}
+
+export interface Links {
+  self: Self
+  attractions: Attraction[]
+  venues: Venue[]
+}
+
 export interface Self {
   href: string
 }
@@ -127,23 +190,39 @@ export interface Embedded2 {
 }
 
 export interface Venue2 {
+  name: string
   type: string
   id: string
   test: boolean
-  url: string
+  url?: string
   locale: string
+  aliases?: string[]
   postalCode: string
   timezone: string
   city: City
+  state: State
   country: Country
   address: Address
   location: Location
+  markets?: Market[]
+  dmas: Dma[]
   upcomingEvents: UpcomingEvents
   _links: Links2
+  images?: Image2[]
+  boxOfficeInfo?: BoxOfficeInfo
+  parkingDetail?: string
+  accessibleSeatingDetail?: string
+  generalInfo?: GeneralInfo
+  social?: Social
 }
 
 export interface City {
   name: string
+}
+
+export interface State {
+  name: string
+  stateCode: string
 }
 
 export interface Country {
@@ -153,6 +232,7 @@ export interface Country {
 
 export interface Address {
   line1: string
+  line2?: string
 }
 
 export interface Location {
@@ -160,10 +240,20 @@ export interface Location {
   latitude: string
 }
 
+export interface Market {
+  name: string
+  id: string
+}
+
+export interface Dma {
+  id: number
+}
+
 export interface UpcomingEvents {
   _total: number
-  "mfx-de": number
+  ticketmaster: number
   _filtered: number
+  tmr?: number
 }
 
 export interface Links2 {
@@ -174,6 +264,34 @@ export interface Self2 {
   href: string
 }
 
+export interface Image2 {
+  ratio: string
+  url: string
+  width: number
+  height: number
+  fallback: boolean
+}
+
+export interface BoxOfficeInfo {
+  phoneNumberDetail?: string
+  openHoursDetail: string
+  acceptedPaymentDetail?: string
+  willCallDetail?: string
+}
+
+export interface GeneralInfo {
+  generalRule: string
+  childRule?: string
+}
+
+export interface Social {
+  twitter: Twitter
+}
+
+export interface Twitter {
+  handle: string
+}
+
 export interface Attraction2 {
   name: string
   type: string
@@ -181,32 +299,32 @@ export interface Attraction2 {
   test: boolean
   url: string
   locale: string
-  externalLinks: ExternalLinks
-  aliases: string[]
-  images: Image2[]
+  externalLinks?: ExternalLinks
+  aliases?: string[]
+  images: Image3[]
   classifications: Classification2[]
   upcomingEvents: UpcomingEvents2
   _links: Links3
 }
 
 export interface ExternalLinks {
-  youtube: Youtube[]
-  twitter: Twitter[]
-  itunes: Itune[]
-  lastfm: Lastfm[]
+  youtube?: Youtube[]
+  twitter?: Twitter2[]
+  itunes?: Itune[]
+  lastfm?: Lastfm[]
   facebook: Facebook[]
-  wiki: Wiki[]
-  spotify: Spotify[]
+  wiki?: Wiki[]
+  spotify?: Spotify[]
+  musicbrainz?: Musicbrainz[]
   instagram: Instagram[]
-  musicbrainz: Musicbrainz[]
-  homepage: Homepage[]
+  homepage?: Homepage[]
 }
 
 export interface Youtube {
   url: string
 }
 
-export interface Twitter {
+export interface Twitter2 {
   url: string
 }
 
@@ -230,19 +348,19 @@ export interface Spotify {
   url: string
 }
 
-export interface Instagram {
-  url: string
-}
-
 export interface Musicbrainz {
   id: string
+}
+
+export interface Instagram {
+  url: string
 }
 
 export interface Homepage {
   url: string
 }
 
-export interface Image2 {
+export interface Image3 {
   ratio: string
   url: string
   width: number
@@ -255,8 +373,8 @@ export interface Classification2 {
   segment: Segment2
   genre: Genre2
   subGenre: SubGenre2
-  type: Type
-  subType: SubType
+  type: Type2
+  subType: SubType2
   family: boolean
 }
 
@@ -275,25 +393,22 @@ export interface SubGenre2 {
   name: string
 }
 
-export interface Type {
+export interface Type2 {
   id: string
   name: string
 }
 
-export interface SubType {
+export interface SubType2 {
   id: string
   name: string
 }
 
 export interface UpcomingEvents2 {
   _total: number
-  tmr: number
-  "mfx-nl": number
-  "mfx-de": number
-  "mfx-es": number
+  tmr?: number
   ticketmaster: number
   _filtered: number
-  "mfx-pl": number
+  ticketweb?: number
 }
 
 export interface Links3 {
@@ -302,6 +417,60 @@ export interface Links3 {
 
 export interface Self3 {
   href: string
+}
+
+export interface Product {
+  name: string
+  id: string
+  url: string
+  type: string
+  classifications: Classification3[]
+}
+
+export interface Classification3 {
+  primary: boolean
+  segment: Segment3
+  genre: Genre3
+  subGenre: SubGenre3
+  type: Type3
+  subType: SubType3
+  family: boolean
+}
+
+export interface Segment3 {
+  id: string
+  name: string
+}
+
+export interface Genre3 {
+  id: string
+  name: string
+}
+
+export interface SubGenre3 {
+  id: string
+  name: string
+}
+
+export interface Type3 {
+  id: string
+  name: string
+}
+
+export interface SubType3 {
+  id: string
+  name: string
+}
+
+export interface Outlet {
+  url: string
+  type: string
+}
+
+export interface DoorsTimes {
+  localDate: string
+  localTime: string
+  dateTime: string
 }
 
 export interface Links4 {
@@ -332,61 +501,4 @@ export interface Page {
   totalElements: number
   totalPages: number
   number: number
-}
-
-
-export interface Embedded {
-  events: Event[]
-}
-
-export interface Event {
-  name: string
-  type: string
-  id: string
-  url: string
-  locale: string
-  images: Image[]
-  dates: Dates
-  priceRanges: PriceRange[]
-  _embedded: Embedded2
-}
-
-export interface Image {
-  url: string
-}
-
-export interface Dates {
-  start: Start
-}
-
-export interface Start {
-  localDate: string
-  localTime: string
-}
-
-export interface PriceRange {
-  currency: string
-  min: number
-  max: number
-}
-
-export interface Attraction {
-  href: string
-}
-
-export interface Embedded2 {
-  venues: Venue2[]
-}
-
-export interface Venue2 {
-  city: City
-  country: Country
-}
-
-export interface City {
-  name: string
-}
-
-export interface Country {
-  name: string
 }
